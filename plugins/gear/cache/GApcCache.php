23fcb840e1f647f488367d1f43e833bb41267b70
DenLinker <denlinkers@gmail.com>
2014-01-30 13:01:52 +0400

    Apc&Memcache plugins complete

--- plugins/gear/cache/GApcCache.php
+++ plugins/gear/cache/GApcCache.php
@@ -29,8 +29,16 @@ class GApcCache extends GCache
      * @param integer $expire
      * @return boolean
      */
-    public function add($key, $value, $expire = 30)
+    public function add($key, $value = null, $expire = 30)
     {
+        if (is_array($key))
+        {
+            if ($value !== null)
+                $expire = (int)$value;
+            apc_add($key, null, $expire);
+        }
+        else
+            return apc_add($key, $value, $expire);
     }
     
     /**
@@ -45,20 +53,41 @@ class GApcCache extends GCache
      */
     public function set($key, $value, $expire = 30)
     {
+        if (is_array($key))
+        {
+            if ($value !== null)
+                $expire = (int)$value;
+            apc_store($key, null, $expire);
+        }
+        else
+            return apc_store($key, $value, $expire);
     }
     
     /**
      * Получение значения из кэша
      * 
-     * @abstract
      * @access public
-     * @param string $key
-     * @param mixed $value
-     * @param integer $expire
-     * @return boolean
+     * @param string|array $key
+     * @param boolean|closure $unserialize
+     * @return mixed
      */
     public function get($key, $unserialize = false)
     {
+        if (is_array($key))
+        {
+            $keys = array();
+            foreach($key as $k => $us)
+                if (!is_bool($us) && !is_callable($us))
+                    $keys[$us] = $unserialize;
+                else
+                    $keys[$k] = $us;
+            $result = apc_fetch(array_keys($keys));
+            foreach($result as $k => $v)
+                if ($keys[$k])
+                    $result[$k] = is_callable($keys[$k]) ? call_user_func($keys[$k], $v) : unserialize($v);
+            return $result;
+        }
+        return apc_fetch($key);
     }
     
     /**
@@ -66,11 +95,12 @@ class GApcCache extends GCache
      * 
      * @abstract
      * @access public
-     * @param string $key
+     * @param string|array $key
      * @return boolean
      */
     public function exists($key)
     {
+        return apc_exists($key);
     }
     
     /**
@@ -78,11 +108,19 @@ class GApcCache extends GCache
      * 
      * @abstract
      * @access public
-     * @param string $key
+     * @param string|array $key
      * @return boolean
      */
     public function remove($key)
     {
+        if (is_array($key))
+        {
+            $size = $result = count($key);
+            foreach($key as $k)
+                $result = apc_delete($k) ? $result - 1 : $result;
+            return !$result ? true : ($result === $size ? false : $result);
+        }
+        return apc_delete($key);
     }
     
     /**
@@ -94,5 +132,32 @@ class GApcCache extends GCache
      */
     public function clear()
     {
+        return apc_clear_cache();
+    }
+    
+    /**
+     * Увеличичвает значение в кэше на $step
+     * 
+     * @access public
+     * @param string $key
+     * @param integer $step
+     * @return boolean
+     */
+    public function inc($key, $step = 1)
+    {
+        return apc_inc($key, $step);
+    }
+    
+    /**
+     * Уменьшает значение в кэше на $step
+     * 
+     * @access public
+     * @param string $key
+     * @param integer $step
+     * @return boolean
+     */
+    public function dec($key, $step = 1)
+    {
+        return apc_dec($key, $step);
     }
 }
--- plugins/gear/cache/GMemCache.php
+++ plugins/gear/cache/GMemCache.php
@@ -49,16 +49,43 @@ class GMemCache extends GCache
     }
     
     /**
+     * Добавление сервера
+     * 
+     * @access public
+     * @param array|object $server
+     * @return void
+     */
+    public function addServer($server)
+    {
+        if (is_array($server))
+            $server = new GModel($server);
+        else
+        if (!($server instanceof GModel))
+            $this->e('Invalid server');
+        $this->_servers[] = $server;
+    }
+    
+    /**
      * Добавление значения в кэш
      * 
      * @access public
-     * @param string $key
-     * @param mixed $value
+     * @param string|array $key array as array(key => value, key => value, ...)
+     * @param mixed $value as $expire when $key is array
      * @param integer $expire
      * @return boolean
      */
-    public function add($key, $value, $expire = 30)
+    public function add($key, $value = null, $expire = 30)
     {
+        if (is_array($key))
+        {
+            if ($value !== null)
+                $expire = (int)$value;
+            $size = $result = count($key);
+            foreach($key as $k => $v)
+                $result = $this->_cache->add($k, $v, 0, $expire ? time() + $expire : 0) ? $result - 1 : $result;
+            return !$result ? true : ($size === $result ? false : $result);
+        }
+        return $this->_cache->add($key, $value, 0, $expire ? time() + $expire : 0);
     }
     
     /**
@@ -66,62 +93,129 @@ class GMemCache extends GCache
      * кэше
      * 
      * @access public
-     * @param string $key
-     * @param mixed $value
+     * @param string|array $key array as array(key => value, key => value, ...)
+     * @param mixed $value as $expire when $key is array
      * @param integer $expire
      * @return boolean
      */
     public function set($key, $value, $expire = 30)
     {
+        if (is_array($key))
+        {
+            if ($value !== null)
+                $expire = (int)$value;
+            $size = $result = count($key);
+            foreach($key as $k => $v)
+                $result = $this->_cache->set($k, $v, 0, $expire ? time() + $expire : 0) ? $result - 1 : $result;
+            return !$result ? true : ($size === $result ? false : $result);
+        }
+        return $this->_cache->set($key, $value, 0, $expire ? time() + $expire : 0);
     }
     
     /**
      * Получение значения из кэша
      * 
-     * @abstract
      * @access public
-     * @param string $key
-     * @param mixed $value
-     * @param integer $expire
-     * @return boolean
+     * @param string|array $key
+     * @param boolean|closure $unserialize
+     * @return mixed
      */
     public function get($key, $unserialize = false)
     {
+        if (is_array($key))
+        {
+            $result = array();
+            foreach($key as $k => $us)
+            {
+                $value = $us === true || is_callable($us) ? $this->_cache->get($k) : $this->_cache->get($us);
+                if (!is_bool($us) && !is_callable($us))
+                {
+                    $k = $us;
+                    $us = $unserialize;
+                }
+                if ($value = $this->_cache->get($k))
+                    $result[] = $us === true ? unserialize($value) : (is_callable($us) ? $us($value) : $value);
+            }
+            return $result;
+        }
+        $value = $this->_cache->get($key);
+        if ($value && $unserialize)
+            $value = $unserialize === true ? unserialize($value) : (is_callable($unserialize) ? $unserialize($value) : $value);
+        return $value;
     }
     
     /**
      * Проверка на наличие в кэше значения под указанным ключём
      * 
-     * @abstract
      * @access public
-     * @param string $key
-     * @return boolean
+     * @param string|array $key
+     * @return boolean|array
      */
     public function exists($key)
     {
+        if (is_array($key))
+        {
+            $result = array();
+            foreach($key as $k)
+                $result[$k] = $this->_cache->get($k) !== false ? true : false;
+        }
+        return $this->_cache->get($key) !== false ? true : false;
     }
     
     /**
      * Удаление значения из кэша
      * 
-     * @abstract
      * @access public
-     * @param string $key
+     * @param string|array $key
      * @return boolean
      */
     public function remove($key)
     {
+        if (is_array($key))
+        {
+            $size = $result = count($key);
+            foreach($key as $k)
+                $result = $this->_cache->delete($k) ? $result - 1 : $result;
+            return !$result ? true : ($result === $size ? false : $result);
+        }
+        return $this->_cache->delete($key);
     }
     
     /**
      * Очистка кэша
      * 
-     * @abstract
      * @access public
      * @return boolean
      */
     public function clear()
     {
+        return $this->_cache->flush();
+    }
+    
+    /**
+     * Увеличичвает значение в кэше на $step
+     * 
+     * @access public
+     * @param string $key
+     * @param integer $step
+     * @return boolean
+     */
+    public function inc($key, $step = 1)
+    {
+        return $this->_cache->increment($key, $step);
+    }
+    
+    /**
+     * Уменьшает значение в кэше на $step
+     * 
+     * @access public
+     * @param string $key
+     * @param integer $step
+     * @return boolean
+     */
+    public function dec($key, $step = 1)
+    {
+        return $this->_cache->decrement($key, $step);
     }
     
     /**
