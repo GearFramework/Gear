80f7692697731941d8ec9e4cdd543ac81415fefd
DenLinker <denlinkers@gmail.com>
2014-06-18 16:45:22 +0400

    factory interface

--- interfaces/IFactory.php
+++ interfaces/IFactory.php
@@ -2,7 +2,23 @@
 
 namespace gear\interfaces;
 
+/** 
+ * Интерфейс фабрики объектов
+ * 
+ * @package Gear Framework
+ * @author Kukushkin Denis
+ * @copyright Kukushkin Denis
+ * @version 0.0.1
+ * @since 18.06.2014
+ */
 interface IFactory
 {
-    
+    /**
+     * Возвращает созданный объект
+     * 
+     * @access public
+     * @param array $properties
+     * @return object
+     */
+    public function factory(array $properties = array());
 }
