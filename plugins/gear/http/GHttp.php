69d01a2ea5dfa81c5ba322e806deabff2c80d218
DenLinker <denlinkers@gmail.com>
2014-12-21 23:38:11 +0400

    Add Http-plugin

---
+++ plugins/gear/http/GHttp.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace gear\plugins\gear\http;
+use gear\Core;
+use gear\library\GPlugin;
+use gear\library\GException;
+
+class GHttp extends GPlugin
+{
+    /* Const */
+    /* Private */
+    /* Protected */
+    protected static $_init = false;
+    /* Public */
+    
+}
+
+class HttpException extends GException
+{
+    /* Const */
+    /* Private */
+    /* Protected */
+    /* Public */
+}
