2c454c465135589ec32224f3b6b3ad8fb80c7947
DenLinker <denlinkers@gmail.com>
2015-08-04 23:39:53 +0400

    + Writing installer + Remove ftp-component into new repository

--- installer/components/GInstallerComponent.php
+++ installer/components/GInstallerComponent.php
@@ -13,12 +13,17 @@ class GInstallerComponent extends GComponent
     protected static $_defaultProperties =
     [
         'userAgent' => 'Gear-Framework',
-        'installationPath' => '\gear\components\gear',
+        'installationPaths' =>
+        [
+            'modules' => '\gear\modules',
+            'components' => '\gear\components\gear',
+            'plugins' => '\gear\plugins\gear',
+            'helpers' => '\gear\helpers',
+        ],
+        'urlApi' => 'https://api.github.com',
+        'repositories' => ['https://www.github.com/GearFramework']
     ];
-    protected $_urlApi = 'https://api.github.com';
     /* Public */
-
-    public function getUrlApi() { return $this->_urlApi; }
 
     public function installResource($resource)
     {
@@ -51,31 +56,72 @@ class GInstallerComponent extends GComponent
 
     public function installComponents($component)
     {
-        if (!$this->isExists($component . '.component'))
+        if ($this->isInstalled('components', $component))
         {
-            echo "ERROR]\n", "Component $component.component not found\n";
+            echo "Component $component already installed\n";
             return false;
         }
-        echo "OK]\nGet listing [";
-        $listing = $this->getListing($component . '.component');
-        if ($listing)
+        if (($found = $this->isExists($component . '-component')) === false)
         {
-            echo "ERROR]\n";
+            echo "Component $component not found\n";
             return false;
         }
+        list($url, $owner, $repo) = $found;
+        echo "Get listing / [";
+        $listing = $this->getListing($repo, '/');
+        if (!$listing) { echo "ERROR]\n"; return false; }
         echo "OK]\n";
-        foreach($listing as $list)
-        {
-            echo $list->path . " download [OK]\n";
-        }
-        return true;
+        $toPath = $this->getInstallationPath('components', $component);
+        echo "Create component directory $toPath [";
+        if (!@mkdir($toPath)) { echo "ERROR]\n"; return false; }
+        echo "OK]\n";
+        echo "Download component to $toPath...\n";
+        return $this->downloadResource($listing, $toPath, $repo);
     }
 
-    public function getListing($resource)
+    public function downloadResource($listing, $toPath, $repo)
+    {
+        $result = true;
+        foreach($listing as $list)
+        {
+            if ($list->type === 'file')
+            {
+                echo $list->path . " download file " . $list->download_url . " [";
+                if (!@file_put_contents($toPath . '/' . $list->name, file_get_contents($list->download_url)))
+                {
+                    $result = false;
+                    echo "ERROR]\n", "Download from " . $list->download_url . " error\n";
+                    break;
+                }
+                echo "OK]\n";
+            }
+            else
+            {
+                $dir = $toPath . '/' . $list->name;
+                echo "Create directory $dir [";
+                if (!@mkdir($dir)) { $result = false; echo "ERROR]\n"; break; }
+                echo "OK]\n";
+                $listingDir = $this->getListing($repo, $list->path);
+                echo "Get listing " . $list->path . " [";
+                if (!$listingDir) { echo "ERROR]\n"; $result = false; break; }
+                echo "OK]\n";
+                echo "Download folder to $dir...\n";
+                if (!$this->downloadResource($listingDir, $dir, $repo)) { $result = false; break; }
+            }
+        }
+        return $result;
+    }
+
+    public function getInstallationPath($type, $name)
+    {
+        return Core::resolvePath($this->installationPaths[$type] . '/' . $name);
+    }
+
+    public function getListing($resource, $path)
     {
         $result = Core::app()->http->get
         (
-            $this->urlApi . '/repos/GearFramework/' . $resource . '/contents',
+            str_replace('/{+path}', $path, $resource->contents_url),
             [],
             ['UserAgent' => $this->userAgent],
             [$this, 'callbackResponse']
@@ -85,16 +131,30 @@ class GInstallerComponent extends GComponent
 
     public function isExists($resource)
     {
-        echo "Search $resource [";
-        $result = Core::app()->http->get
-        (
-            $this->urlApi . '/repos/GearFramework/' . $resource,
-            [],
-            ['UserAgent' => $this->userAgent],
-            [$this, 'callbackResponse']
-        );
-        return !is_object($result) || isset($result->message) ? false : true;
+        $found = false;
+        foreach($this->repositories as $url)
+        {
+            echo "Search $resource in $url [";
+            $owner = substr($url, strrpos($url, '/') + 1);
+            $result = Core::app()->http->get
+            (
+                $this->urlApi . '/repos/' . $owner . '/' . $resource,
+                [],
+                ['UserAgent' => $this->userAgent],
+                [$this, 'callbackResponse']
+            );
+            if (is_object($result) && !isset($result->message))
+            {
+                $found = [$url, $owner, $result];
+                echo "FOUND]\n";
+                break;
+            }
+            echo "NOT FOUND]\n";
+        }
+        return $found;
     }
+
+    public function isInstalled($type, $name) { return file_exists($this->getInstallationPath($type, $name)); }
 
     public function callbackResponse($response)
     {
