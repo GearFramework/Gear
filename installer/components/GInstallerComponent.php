1a86d4e89005d6bb0e6670dc06608e83d18a5150
DenLinker <denlinkers@gmail.com>
2015-08-05 16:36:57 +0400

    + added updating functionally

--- installer/components/GInstallerComponent.php
+++ installer/components/GInstallerComponent.php
@@ -17,6 +17,9 @@ class GInstallerComponent extends GComponent
     const HELPER_ALREADY = 'Helper :helperName already installed';
     const RESOURCE_ALREADY = ':resourceType :resourceName already installed';
 
+    const RESOURCE_NOT_INSTALLED = 'Updating :resourceType :resourceName not installed';
+    const RESOURCE_INSTALLED_PATH = ':resourceType :resourceName installed into :dirName';
+
     const MODULE_NOT_FOUND = 'Module :moduleName not found';
     const COMPONENT_NOT_FOUND = 'Component :componentName not found';
     const PLUGIN_NOT_FOUND = 'Plugin :pluginName not found';
@@ -28,6 +31,7 @@ class GInstallerComponent extends GComponent
     const PLUGIN_DOWNLOAD = 'Download plugin :pluginName to :dirName';
     const HELPER_DOWNLOAD = 'Download helper :helperName to :dirName';
     const RESOURCE_DOWNLOAD = 'Download :resourceType :resourceName to :dirName';
+    const RESOURCE_UPDATE = 'Update :resourceType :resourceName in :dirName';
 
     const GET_LISTING = 'Get listing :dirName';
     const CREATE_DIR = 'Create directory :dirName';
@@ -99,7 +103,7 @@ class GInstallerComponent extends GComponent
             $this->log(self::STATUS_OK);
         }
         $this->log(self::RESOURCE_DOWNLOAD, ['resourceType' => $type, 'resourceName' => $resourceName, 'dirName' => $toPath]);
-        return $this->downloadResource($listing, $toPath, $repo);
+        return $this->downloadInstallingResource($listing, $toPath, $repo);
     }
 
     /**
@@ -111,77 +115,36 @@ class GInstallerComponent extends GComponent
      */
     public function updateResource($resource)
     {
-        list($type, $name) = explode('/', $resource);
-        $method = 'update' . ucfirst(strtolower($type));
-        if (!method_exists($this, $method))
+        list($type, $resourceName) = explode('/', $resource);
+        $type = strtolower($type);
+        if (!in_array($type, ['module', 'component', 'plugin', 'helper']))
             $this->e(self::INVALID_TYPE_UPDATE);
-        return $this->$method($name);
-    }
+        $method = 'update' . ucfirst($type);
+        if (method_exists($this, $method))
+            return $this->$method($resourceName);
 
-    /**
-     * Установка указанного модуля
-     *
-     * @access public
-     * @param string $component
-     * @return bool
-     */
-    public function installModules($module)
-    {
-    }
-
-    /**
-     * Установка указанного компонента
-     *
-     * @access public
-     * @param string $component
-     * @return bool
-     */
-/*    public function installComponents($component)
-    {
-        if ($this->isInstalled('components', $component))
-            $this->e(self::COMPONENT_ALREADY, ['componentName' => $component]);
-        if (($found = $this->isExists($component . '-component')) === false)
-            $this->e(self::COMPONENT_NOT_FOUND, ['componentName' => $component]);
+        if (!$this->isInstalled($type, $resourceName))
+            $this->e(self::RESOURCE_NOT_INSTALLED, ['resourceType' => $type, 'resourceName' => $resourceName]);
+        if (($found = $this->isExists($resourceName . '-' . $type)) === false)
+            $this->e(self::RESOURCE_NOT_FOUND, ['resourceType' => ucfirst($type), 'resourceName' => $resourceName]);
         list($url, $owner, $repo) = $found;
         $this->log(self::GET_LISTING, ['dirName' => '/'], false);
         $listing = $this->getListing($repo, '/');
         if (!$listing)
             $this->e(self::STATUS_ERROR);
         $this->log(self::STATUS_OK);
-        $toPath = $this->getInstallationPath('components', $component, $listing);
-        if (!file_exists($toPath))
-        {
-            $this->log(self::CREATE_DIR, ['dirName' => $toPath], false);
-            if (!@mkdir($toPath))
-                $this->e(self::STATUS_ERROR);
-            $this->log(self::STATUS_OK);
-        }
-        $this->log(self::COMPONENT_DOWNLOAD, [':componentName' => $component, 'dirName' => $toPath]);
-        return $this->downloadResource($listing, $toPath, $repo);
-    }*/
-
-    /**
-     * Установка указанного плагина
-     *
-     * @access public
-     * @param string $plugin
-     * @return bool
-     */
-    public function installPlugins($plugin)
-    {
+        $toPath = $this->getInstallationPath($type, $resourceName, $listing);
+        $this->log(self::RESOURCE_INSTALLED_PATH, ['resourceType' => ucfirst($type), 'resourceName' => $resourceName, 'dirName' => $toPath]);
+        $this->checkNeedleUpdating($toPath);
+        $this->log(self::RESOURCE_UPDATE, ['resourceType' => $type, 'resourceName' => $resourceName, 'dirName' => $toPath]);
+        return $this->downloadUpdatingResource($listing, $toPath, $repo);
 
     }
 
-    /**
-     * Установка указанного хелпера
-     *
-     * @access public
-     * @param string $helper
-     * @return bool
-     */
-    public function installHelpers($helper)
+    public function checkNeedleUpdating($installedPath)
     {
-
+        print_r($this->_resourceSettings);
+        die();
     }
 
     /**
@@ -193,7 +156,7 @@ class GInstallerComponent extends GComponent
      * @param object $repo
      * @return bool
      */
-    public function downloadResource($listing, $toPath, $repo)
+    public function downloadInstallingResource($listing, $toPath, $repo)
     {
         $this->log(self::DOWNLOAD_DIR, ['dirName' => $toPath]);
         $result = true;
@@ -218,7 +181,52 @@ class GInstallerComponent extends GComponent
                 if (!$listingDir)
                     $this->e(self::STATUS_ERROR);
                 $this->log(self::STATUS_OK);
-                $this->downloadResource($listingDir, $dir, $repo);
+                $this->downloadInstallingResource($listingDir, $dir, $repo);
+            }
+        }
+        return $result;
+    }
+
+    /**
+     * Обновление-загрузка указанного ресурса
+     *
+     * @access public
+     * @param array $listing
+     * @param string $toPath
+     * @param object $repo
+     * @return bool
+     */
+    public function downloadUpdatingResource($listing, $toPath, $repo)
+    {
+        $this->log(self::DOWNLOAD_DIR, ['dirName' => $toPath]);
+        $result = true;
+        foreach($listing as $list)
+        {
+            if ($list->type === 'file')
+            {
+                $this->log(self::DOWNLOAD_FILE, ['fileName' => $list->download_url], false);
+                if (file_exists($toPath . '/' . $list->name))
+                    @unlink($toPath . '/' . $list->name);
+                if (!@file_put_contents($toPath . '/' . $list->name, file_get_contents($list->download_url)))
+                    $this->e(self::STATUS_ERROR);
+                $this->log(self::STATUS_OK);
+            }
+            else
+            {
+                $dir = $toPath . '/' . $list->name;
+                $this->log(self::CREATE_DIR, ['dirName' => $dir], false);
+                if (!file_exists($dir))
+                {
+                    if (!@mkdir($dir))
+                        $this->e(self::STATUS_ERROR);
+                    $this->log(self::STATUS_OK);
+                }
+                $listingDir = $this->getListing($repo, $list->path);
+                $this->log(self::GET_LISTING, ['dirName' => $list->path], false);
+                if (!$listingDir)
+                    $this->e(self::STATUS_ERROR);
+                $this->log(self::STATUS_OK);
+                $this->downloadUpdatingResource($listingDir, $dir, $repo);
             }
         }
         return $result;
