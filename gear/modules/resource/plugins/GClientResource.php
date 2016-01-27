0b74df28419b7025fdc9dcadfdb3842e0e26a460
denisk <denlinkers@gmail.com>
2016-01-27 22:38:52 +0300

    - bug fix

--- gear/modules/resource/plugins/GClientResource.php
+++ gear/modules/resource/plugins/GClientResource.php
@@ -96,7 +96,6 @@ abstract class GClientResource extends GPlugin
         Core::syslog(__CLASS__ . ' -> Resource path ' . $resourcePath . '[' . __LINE__ . ']');
         $hash = md5($resourcePath);
         Core::syslog(__CLASS__ . ' -> Resource hash ' . $hash . '[' . __LINE__ . ']');
-        die();
         if ($render) {
             Core::syslog(__CLASS__ . ' -> Rendering resource [' . __LINE__ . ']');
             $content = $this->owner->view->render($resourcePath, array(), true);
