37abffb6e287e9d410107a57b9abf35df9308d63
denisk <denlinkers@gmail.com>
2017-02-20 13:43:53 +0300

    Develop databases

--- gear/components/db/mysql/GMySqlCursor.php
+++ gear/components/db/mysql/GMySqlCursor.php
@@ -28,6 +28,9 @@ class GMySqlCursor extends GDbCursor
     use TFactory;
     /* Const */
     /* Private */
+    private $_logics = ['$or' => 'OR', '$and' => 'AND'];
+    private $_eqs = ['$lt' => '<', '$gt' => '>', '$ne' => '<>', '$lte' => '<=', '$gte' => '>='];
+    private $_funcs = ['$isn' => 'IS NULL', '$isnn' => 'IS NOT NULL'];
     /* Protected */
     protected $_queryBuild = [];
     protected $_factoryQueryBuild = [
@@ -338,6 +341,9 @@ class GMySqlCursor extends GDbCursor
 
     /**
      * $this->where(['a' => 2]);
+     * $this->where(['$ne' => ['a' => 2]]);
+     * $this->where(['a' => 'NOW()']);
+     * $this->where(['a' => ':b']);
      * $this->where(['a' => ['&lt' => 2]]);
      * $this->where(['a' => [2, 3, 4]]);
      * $this->where(['a' => ['$lt' => 2, '$or' => 3]]);
@@ -352,27 +358,34 @@ class GMySqlCursor extends GDbCursor
     private function _prepareCriteria(array $criteria, $logic = 'AND', $op = null, $eq = '='): string
     {
         $result = [];
-        $currentRow = '';
         foreach($criteria as $left => $right) {
             if (is_numeric($left)) {
                 $result[] = '(' . $this->_prepareCriteria($right, $logic) . ')';
-            } else if (in_array($left, ['$or', '$and'])) {
-
-            } else if (in_array($left, ['$lt', '$gt', '$ne', '$lte', '$gte'])) {
-
-            } else if ($left === '$in') {
-                $result[] = " $logic $op IN " . implode(', ', $this->_prepareValue($right)) . ')';
-            } else if ($left === '$nin') {
-                $result[] = " $logic $op NOT IN " . implode(', ', $this->_prepareValue($right)) . ')';
-            } else {
-                if (ArrayHelper::isAssoc($right)) {
-                    $result[] = "$left IN (" . implode(', ', $this->_prepareValue($right)) . ')';
+            } else if (in_array($left, array_keys($this->_logics))) {
+                $result[] = $this->_logics[$left] . ' ' . $this->_prepareCriteria($right, $this->_logics[$left]);
+            } else if (in_array($left, array_keys($this->_eqs))) {
+                if (!$op) {
+                    $result[] = $this->_prepareCriteria($right, $logic, null, $eq); // ['$lt' => ['a' => 3]]
                 } else {
-                    $result[] = " $logic " . $this->_prepareCriteria($right, $logic, $left);
+                    $result[] = "$op " . $this->_eqs[$left] . " " . $this->_prepareValue($right); // ['a' => ['$lt' => 3]]
+                }
+            } else if ($left === '$in') {
+                $result[] = ($result ? $logic : "") . " $op IN (" . implode(', ', $this->_prepareValue($right)) . ')';
+            } else if ($left === '$nin') {
+                $result[] = ($result ? $logic : "") . " $op NOT IN (" . implode(', ', $this->_prepareValue($right)) . ')';
+            } else {
+                if (is_array($right)) {
+                    if (!ArrayHelper::isAssoc($right)) {
+                        $result[] = ($result ? $logic : "") . " $left IN (" . implode(', ', $this->_prepareValue($right)) . ')';
+                    } else {
+                        $result[] = ($result ? $logic : "") . " " . $this->_prepareCriteria($right, $logic, $left);
+                    }
+                } else {
+                    $result[] = ($result ? $logic : "") . " $left $eq " . $this->_prepareValue($right);
                 }
             }
         }
-        return implode(" $logic ", $result);
+        return implode(" ", $result);
     }
 
     /**
@@ -413,14 +426,43 @@ class GMySqlCursor extends GDbCursor
     private function _prepareValue($value)
     {
         if (is_array($value)) {
-            foreach($value as &$val) {
+            foreach ($value as &$val) {
                 $val = $this->_prepareValue($val);
             }
             unset($val);
+        } else if ($value === null || preg_match('/^null$/i', $value)) {
+            $value = 'NULL';
+        } else if (($operand = $this->_prepareOperand($value))) {
+            $value = $operand;
         } else {
             $value = "'" . $this->escape($value) . "'";
         }
         return $value;
+    }
+
+    private function _prepareOperand($operand)
+    {
+        if (!is_numeric($operand)) {
+            if ($operand === null || preg_match('/^null$/i', $operand)) {
+                $operand = 'NULL';
+            } else if (preg_match('/^[a-z0-9_]+\.[a-z0-9_]+$/i', $operand)) {
+                $rec = explode('.', $operand);
+                $operand = '`' . implode('`.`', $rec) . '`';
+            } else if (strpos($operand, ' AS ') !== false) {
+                $operand = preg_replace('/\s{2,}/', ' ', $operand);
+                list($left, $alias) = explode(' AS ', $operand);
+                $operand = (preg_match('/^[A-Z0-9_]+$/i', $left) ? "`$left`" : $left) . " AS `$alias`";
+            } else if ($operand[0] === ':') { // Column in table $operand == ':id'
+                $operand = '`' . substr($operand, 1) . '`';
+            } else if ($operand[0] === '$') { // Function $operand == '$SUM(price)'
+                $operand = substr($operand, 1);
+            } else {
+                $operand = false;
+            }
+        } else {
+            $operand = false;
+        }
+        return $operand;
     }
 
     /**
