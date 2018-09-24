84be675a4a0bb0a13a6117606d474398da735134
denisk <denlinkers@gmail.com>
2018-09-24 14:27:30 +0300

    Fix comment

--- Gear/Traits/TFactory.php
+++ Gear/Traits/TFactory.php
@@ -13,6 +13,9 @@ use Gear\Library\GEvent;
  * @author Kukushkin Denis
  * @copyright 2016 Kukushkin Denis
  * @license http://www.spdx.org/licenses/MIT MIT License
+ *
+ * @property array factoryProperties
+ *
  * @since 0.0.1
  * @version 0.0.1
  */
