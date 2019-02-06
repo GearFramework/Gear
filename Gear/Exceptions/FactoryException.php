f217327c0971b7f76d5f320e32998fa52c07e724
denisk_svn <denlinkers@gmail.com>
2019-02-06 13:00:10 +0300

    Remove sources

--- Gear/Exceptions/FactoryException.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-class FactoryException extends \Gear\Library\GException {}
-class FactoryInvalidItemPropertiesException extends FactoryException
-{
-    public $defaultMessage = 'Item properties must be a an array';
-}
