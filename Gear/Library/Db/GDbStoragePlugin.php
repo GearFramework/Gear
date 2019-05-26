4ec18cb1b3c631ed2a0d64d0a7b465b75e7a3845
Linker <denlinkers@gmail.com>
2019-05-26 17:29:05 +0300

    Refactoring

--- Gear/Library/Db/GDbStoragePlugin.php
+++ Gear/Library/Db/GDbStoragePlugin.php
@@ -3,6 +3,7 @@
 namespace Gear\Library\Db;
 
 use Gear\Interfaces\DbConnectionInterface;
+use Gear\Interfaces\DbStorageComponentInterface;
 use Gear\Interfaces\FactoryInterface;
 use Gear\Library\GPlugin;
 use Gear\Traits\Factory\DelegateFactoryTrait;
@@ -27,7 +28,7 @@ use Gear\Traits\Factory\FactoryTrait;
  * @since 0.0.1
  * @version 0.0.2
  */
-abstract class GDbStoragePlugin extends GPlugin implements \IteratorAggregate, FactoryInterface
+abstract class GDbStoragePlugin extends GPlugin implements \IteratorAggregate, FactoryInterface, DbStorageComponentInterface
 {
     /* Traits */
     use DelegateFactoryTrait;
